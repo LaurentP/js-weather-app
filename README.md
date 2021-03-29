edec172059336d8eb8976cac1a7b524578d8f9c1
LaurentP <lorenzopas@gmail.com>
2021-03-29 18:42:27 +0200

    Added wind and humidity display, added background changes for day and night

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # JS Weather App
 
-Application de météo en JavaScript. L'API OpenWeatherMap est utilisée avec fetch() pour les données. La ville sélectionnée est sauvegardée avec localStorage.
+Application de météo en JavaScript. L'API OpenWeatherMap est utilisée avec fetch() pour les données.
+
+Le nom de la ville choisie est sauvegardé avec localStorage, et l'application s'actualise automatiquement au lancement.
+
+Les informations affichées sont la température, l'état du ciel, la vitesse du vent, et le pourcentage d'humidité.
+
+La couleur du fond change selon qu'il fasse jour ou qu'il fasse nuit dans la ville choisie.
 
 Icônes utilisées pour l'affichage : [https://github.com/manifestinteractive/weather-underground-icons](https://github.com/manifestinteractive/weather-underground-icons)
--- js/app.js
+++ js/app.js
@@ -5,57 +5,70 @@ const API_KEY = '7939b649e70282572b1ac38aa8190594'
 
 // Icons : https://github.com/manifestinteractive/weather-underground-icons
 const WEATHER_ICONS = {
-    '01d': 'clear',
-    '01n': 'nt_clear',
-    '02d': 'mostlysunny',
-    '02n': 'nt_mostlysunny',
-    '03d': 'cloudy',
-    '03n': 'nt_cloudy',
-    '04d': 'cloudy',
-    '04n': 'nt_cloudy',
-    '09d': 'rain',
-    '09n': 'nt_rain',
-    '10d': 'chancerain',
-    '10n': 'nt_chancerain',
-    '11d': 'chancetstorms',
-    '11n': 'nt_chancetstorms',
-    '13d': 'snow',
-    '13n': 'nt_snow',
-    '50d': 'fog',
-    '50n': 'nt_fog'
+    "01d": "clear",
+    "01n": "nt_clear",
+    "02d": "mostlysunny",
+    "02n": "nt_mostlysunny",
+    "03d": "cloudy",
+    "03n": "nt_cloudy",
+    "04d": "cloudy",
+    "04n": "nt_cloudy",
+    "09d": "rain",
+    "09n": "nt_rain",
+    "10d": "chancerain",
+    "10n": "nt_chancerain",
+    "11d": "chancetstorms",
+    "11n": "nt_chancetstorms",
+    "13d": "snow",
+    "13n": "nt_snow",
+    "50d": "fog",
+    "50n": "nt_fog"
 }
 
 const weatherDetails = document.querySelector('.details')
 
 const weatherLoad = (city) => {
-    if (city === '') return
-    let url = API_URL + city + '&appid=' + API_KEY
+    if (city === null || city === undefined) return
+    const url = API_URL + city + '&appid=' + API_KEY
     fetch(url).then(response => {
         if (response.ok) {
             response.json().then(data => {
-                
+
                 // Debug
                 // console.log(data)
-                
+
                 document.querySelector('.city').innerHTML = `<span class="city">${city}</span>`
-                document.querySelector('.icon').innerHTML = `<img src="./img/${WEATHER_ICONS[data.weather[0].icon]}.svg" alt="">`
+
+                const icon = data.weather[0].icon
+
+                document.querySelector('.icon').innerHTML = `<img src="./img/${WEATHER_ICONS[icon]}.svg" alt="">`
+
+                if (/d$/.test(icon)) {
+                    document.body.className = 'day'
+                }
+                else if (/n$/.test(icon)) {
+                    document.body.className = 'night'
+                }
+
                 weatherDetails.innerHTML = `
                     <span class="temp">${Math.ceil(data.main.temp)} °C</span>
                     <span class="description">${data.weather[0].description}</span>
+                    <span>Wind : ${Math.round(data.wind.speed * 3.6)} km/h &bull; Humidity : ${data.main.humidity}%</span>
                 `
             })
         }
         else {
             document.querySelector('.city').innerHTML = null
+            document.body.className = null
             document.querySelector('.icon').innerHTML = '<img src="./img/unknown.svg" alt="">'
-            weatherDetails.innerHTML = null
+            weatherDetails.innerHTML = '<span class="description">Please indicate a city</span>'
         }
     })
 }
 
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault()
-    let city = document.querySelector('form #city').value
+    const city = document.querySelector('form #city').value
     weatherLoad(city)
     localStorage.setItem('city', city)
 })
